86bb50e75bda3413fc7735da39246de65bb31c2f
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-12 14:54:57 -0500

    Finished TIY8

--- TIYs/TIY8/pizzeria/pizzas/models.py
+++ TIYs/TIY8/pizzeria/pizzas/models.py
@@ -4,15 +4,21 @@ from django.db import models
 class Pizza(models.Model):
     """Type of pizza the pizzeria has."""
     text = models.CharField(max_length=200) # Pizza Type
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """Make a string representation of the module."""
+        """Return a string representation of the module."""
         return self.text
 
 
 class Topping(models.Model):
+    """Toppings for the Pizza."""
     topic = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'toppings'
 
     def __str__(self):
         """Return a string representation of the model."""
--- TIYs/TIY8/pizzeria/pizzas/urls.py
+++ TIYs/TIY8/pizzeria/pizzas/urls.py
@@ -6,5 +6,8 @@ from . import views
 app_name = 'pizzas'
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('pizzas/', views.pizzas, name='pizzas'),
+    # Detail page for a single topic
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
